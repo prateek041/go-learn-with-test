bdaabee4619d1fa01a4325e88a70747e008e8500
root <root@kalip.kalis>
2022-11-19 02:38:12 -0500

    done with iterations

--- iterations/iteration.go
+++ iterations/iteration.go
@@ -1,1 +1,8 @@
-//
+package iterations
+
+func Repeat(character string)(word string){
+	for i := 0; i < 5; i++ {
+		word += character
+	}
+	return 
+}
--- iterations/iteration_test.go
+++ iterations/iteration_test.go
@@ -3,10 +3,16 @@ package iterations
 import "testing"
 
 func TestIteration(t *testing.T){
-	got := Repeat('a')
+	got := Repeat("a")
 	want := "aaaaa"
 
 	if got != want{
-		t.Errof("Expected %q but got %q", got, want)
+		t.Errorf("Expected %q but got %q", got, want)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B){
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
 	}
 }
