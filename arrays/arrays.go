4c4e7a11df8ee6655c9ebd840c1162239576a4ed
root <root@kalip.kalis>
2022-11-19 11:21:26 -0500

    arrays: TestSumAll working with two inputs

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -6,3 +6,19 @@ func SumArray(numbers []int)(sum int){
 	}
 	return
 }
+
+func SumAll(firstArray []int, secondArray []int)(arraySum [2]int){
+	firstSum := 0
+	secondSum := 0
+	for _, number := range(firstArray){
+		firstSum += number
+	}
+
+	for _, number := range(secondArray){
+		secondSum += number
+	}
+
+	arraySum[0] = firstSum
+	arraySum[1] = secondSum
+	return
+}
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -12,6 +12,20 @@ func TestSumArray(t *testing.T){
 	})
 }
 
+func TestSumAll(t *testing.T){
+	t.Run("Sum of two different arrays", func(t *testing.T){
+		firstArray := []int{1, 2}
+		secondArray  := []int{0, 9}
+
+		got := SumAll(firstArray, secondArray)
+		want := [2]int{3, 9}
+
+		if got!=want{
+			t.Errorf("wanted %v got %v when passed %v and %v", want, got, firstArray, secondArray)
+		}
+	})
+}
+
 func assertMessageConstructor(t testing.TB, got, want int, numbers []int){
 	t.Helper()
 	if got!=want{
