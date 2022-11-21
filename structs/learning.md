d3d64fe1e42aea558e210625eceec4130852b721
root <root@kalip.kalis>
2022-11-21 06:07:25 -0500

    basic area implementation done

---
+++ structs/learning.md
@@ -0,0 +1,7 @@
+# Learnings about structs, types and Interfaces
+
+### types
+- We use types to **encapsulate** logic together, best example is, area, perimeter etc of different shapes.
+- There is different formulae for different shape in order to calculate these properties.
+- So, in order to make a bunch of functions, why not make something that is self containing ? want to calculate area of a triangle ? use triangle.area() and similarly for rectangle, use rectangle.area()
+- That is where types come in, you can attach methods to them.
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -2,15 +2,28 @@ package structs
 
 import "testing"
 
+type Rectangle struct {
+	width float64
+	height float64
+}
+
 func TestPerimeter(t *testing.T){
+	var width float64 = 10.0 // this is general/test width and height of triangle
+	var height float64 = 10.0
 	t.Run("perimeter of rectangle", func(t *testing.T){
-		var width float64 = 10.0
-		var height float64 = 10.0
 		got := Perimeter(width, height)
 		want := 40.0
 
-		if(want!=got){
+		if want!=got{
 			t.Errorf("got %.2f wanted %.2f", got, want) // here the %.2f is a placeholder where .2 means 2 decimal points
+		}
+	})
+	t.Run("Area of a rectangle", func(t *testing.T){
+		got := Area(width, height)
+		want := 100.0
+
+		if want!= got{
+			t.Errorf("got %.2f wanted %.2f", got, want)
 		}
 	})
 }
